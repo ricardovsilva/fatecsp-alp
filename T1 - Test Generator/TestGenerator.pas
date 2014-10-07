c44bd92aca7c4ed018fb03dfaeeb0d269cd131f1
Ricardo <ricardovsilva@msn.com>
2014-10-07 10:43:21 -0300

    Created Record of product

--- T1 - Test Generator/TestGenerator.pas
+++ T1 - Test Generator/TestGenerator.pas
@@ -1,4 +1,21 @@
 Program TestGenerator ;
-Begin
+  Type
+    Product = Record
+      Code : String[4];
+      Name : String[30];
+      Price : Real;
+  End;
   
+  var  
+    teste : Product;
+Begin 
+  teste.Code := '1234';
+  teste.Name := 'Any product';
+  teste.Price := 2.45;
+  
+  Writeln(teste.Code);
+  WriteLn(teste.Name);
+  WriteLn(teste.Price);
+  
+  read() ; 
 End.
