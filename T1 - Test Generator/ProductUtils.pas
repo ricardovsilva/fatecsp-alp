07ce150d2241b64c0e134fbcda555a2428f3bd6a
ricardovsilva <ricardovsilva@msn.com>
2014-10-17 09:40:24 -0300

    Moved all product functions to other file

---
+++ T1 - Test Generator/ProductUtils.pas
@@ -0,0 +1,85 @@
+unit Product;
+
+interface
+  Type
+      Product = Record
+      Code : String[4];
+      Name : String[30];
+      Price : Real;
+  End;
+  Type
+    Products = array of Product;
+    
+  function ParseProduct(productInfo : SplitedText) : Product;
+  function GetsProducts(filePath : String[255]) : Products;
+  function LineIsValid(line : String[255]): boolean;
+  
+  procedure PrintProducts(productsArray : Products);
+    
+implementation
+  (*Parse array of strings into Product struct*)  
+  function ParseProduct(productInfo : SplitedText) : Product;
+  var
+    returnProduct : Product;
+    codeIndex, nameIndex, priceIndex : integer;
+  Begin
+    codeIndex := 1;
+    nameIndex := 2;
+    priceIndex := 3;
+    
+    returnProduct.Code := productInfo[codeIndex];
+    returnProduct.Name := productInfo[nameIndex];
+    returnProduct.Price := StrToReal(productInfo[priceIndex]);
+    
+    ParseProduct := returnProduct;
+  End;  
+  
+  (*Gets lines of products from file passed by parameter*) 
+  function GetsProducts(filePath : String[255]) : Products;
+  var
+    productsVar : Products;
+    counter : integer;
+    line: string[255];
+    separator: char;
+    fileVar : Text;
+  Begin
+    counter := 1;
+    separator := ';';
+    
+    Assign(fileVar, filePath);
+    Reset(fileVar);
+   
+    while not EOF(fileVar) do
+    begin
+      Readln(fileVar, line);
+      if LineIsValid(line) then
+      begin
+        productsVar[counter] := ParseProduct(Split(line, separator));
+        counter := counter + 1;
+      end
+    end;
+    
+    GetsProducts := productsVar;
+  End;
+  
+  (*Verify if line is not an comment*)
+  function LineIsValid(line : String[255]): boolean;
+  Begin
+    LineIsValid:= not StringIsEmpty(line) and not StringStartsWith(line, '#');
+  end;
+     
+  (*Prints all products of array*)
+  procedure PrintProducts(productsArray : Products);
+  var
+    i : integer;
+  begin
+    for i:= 1 to 15 do
+    begin
+      begin
+        Writeln('Code: ', productsArray[i].Code);
+        Writeln('Description: ', productsArray[i].Name);
+        WriteLn('Price: ', productsArray[i].Price:6:2);
+        WriteLn;
+      end
+    end
+  end;
--- T1 - Test Generator/TestGenerator.pas
+++ T1 - Test Generator/TestGenerator.pas
@@ -2,13 +2,6 @@ Program TestGenerator ;
   uses StringUtils;
   
   Type
-      Product = Record
-      Code : String[4];
-      Name : String[30];
-      Price : Real;
-  End;
-  
-  Type
     Sell = Record
     Datetime : String[10];
     ProductCode : String[4];
@@ -18,59 +11,6 @@ Program TestGenerator ;
   
   Type
     Sells = array[1..65532] of Sell;
-  
-  Type
-    Products = array[1..15] of Product;
-  
-  (*Parse array of strings into Product struct*)  
-  function ParseProduct(productInfo : SplitedText) : Product;
-    var
-      returnProduct : Product;
-      codeIndex, nameIndex, priceIndex : integer;
-    Begin
-      codeIndex := 1;
-      nameIndex := 2;
-      priceIndex := 3;
-      
-      returnProduct.Code := productInfo[codeIndex];
-      returnProduct.Name := productInfo[nameIndex];
-      returnProduct.Price := StrToReal(productInfo[priceIndex]);
-      
-      ParseProduct := returnProduct;
-    End; 
-    
-  function LineIsValid(line : String[255]): boolean;
-  Begin
-    LineIsValid:= not StringIsEmpty(line) and not StringStartsWith(line, '#');
-  end;
-  
-  (*Gets lines of products from file passed by parameter*) 
-  function GetsProducts(filePath : String[255]) : Products;
-  var
-    productsVar : Products;
-    counter : integer;
-    line: string[255];
-    separator: char;
-    fileVar : Text;
-  Begin
-    counter := 1;
-    separator := ';';
-    
-    Assign(fileVar, filePath);
-    Reset(fileVar);
-   
-    while not EOF(fileVar) do
-    begin
-      Readln(fileVar, line);
-      if LineIsValid(line) then
-      begin
-        productsVar[counter] := ParseProduct(Split(line, separator));
-        counter := counter + 1;
-      end
-    end;
-    
-    GetsProducts := productsVar;
-  End;
   
   (*Requests to user quantity of random sells that will be generated*)
   function RequestQuantityOfSells : integer;
@@ -116,22 +56,6 @@ Program TestGenerator ;
     begin
       Writeln(GenerateRandomDay(monthNumber));      
     end; 
-  end;
-  
-  (*Prints all products of array*)
-  procedure PrintProducts(productsArray : Products);
-  var
-    i : integer;
-  begin
-    for i:= 1 to 15 do
-    begin
-      begin
-        Writeln('Code: ', productsArray[i].Code);
-        Writeln('Description: ', productsArray[i].Name);
-        WriteLn('Price: ', productsArray[i].Price:6:2);
-        WriteLn;
-      end
-    end
   end;
 
 var
