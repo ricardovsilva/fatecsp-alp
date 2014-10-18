fc56a90a2397231d0bf9e3cf6d613c53a6ab2bbd
ricardovsilva <ricardovsilva@msn.com>
2014-10-18 15:05:09 -0300

    Added method StrToInteger

--- T1 - Test Generator/StringUtils.pas
+++ T1 - Test Generator/StringUtils.pas
@@ -1,75 +1,87 @@
 unit StringUtils;
 
 interface 
-Type
-  SplitedText = array[1..255] of String[255];
+  Type
+    SplitedText = array[1..255] of String[255];
+  
+  function Split(textToSplit : String[255]; separatorChar : char) : SplitedText;
+  function StringIsEmpty(textToVerify : String[255]) : boolean;
+  function StringStartsWith(stringToVerify: string[255]; startChar : char) : boolean;
+  function StrToInteger (s: String) : integer;
+  function StrToReal (s: String): Real;
 
-function StrToReal (s: String): Real;
-function Split(textToSplit : String[255]; separatorChar : char) : SplitedText;
-function StringIsEmpty(textToVerify : String[255]) : boolean;
-function StringStartsWith(stringToVerify: string[255]; startChar : char) : boolean;
-
-implementation
-(*Converts string to real. This function was copied from 
-http://professorwellingtontelles.blogspot.com.br/2011/06/converter-string-em-real-em-pascal.html
-at 2014-10-07*)
-function StrToReal (s: String): Real;  
-var  
-  r : Real;  
-  p : Integer;  
-begin  
-  Val (s,r,p);  
-  if p > 0 then Val (copy(s,1,p-1),r,p);  
-    StrToReal := r;  
-end;
-
-(*Split text passed by parameter using separator char to do split.*)
-function Split(textToSplit : String[255]; separatorChar : char) : SplitedText;
-var
-  i, offset, counter : integer;
-  returnArray : SplitedText;
-  breakedText : string[255];
-Begin
-  counter := 1;
-  offset := 1;
-  for i:= 1 to Length(textToSplit) do
-  begin
-    if textToSplit[i] = separatorChar then
+implementation 
+  (*Split text passed by parameter using separator char to do split.*)
+  function Split(textToSplit : String[255]; separatorChar : char) : SplitedText;
+  var
+    i, offset, counter : integer;
+    returnArray : SplitedText;
+    breakedText : string[255];
+  Begin
+    counter := 1;
+    offset := 1;
+    for i:= 1 to Length(textToSplit) do
     begin
-      breakedText := copy(textToSplit, offset, i - offset);
-      offset := offset + length(breakedText) + 1;
-      if length(breakedText) > 0 then
+      if textToSplit[i] = separatorChar then
       begin
-        returnArray[counter] := breakedText;
-        counter := counter + 1;
+        breakedText := copy(textToSplit, offset, i - offset);
+        offset := offset + length(breakedText) + 1;
+        if length(breakedText) > 0 then
+        begin
+          returnArray[counter] := breakedText;
+          counter := counter + 1;
+        end
       end
-    end
-  end;
+    end;
+      
+    Split := returnArray;
+  End;
+  
+  (*Verify if string passed by parameter contains only empty chars*)
+  function StringIsEmpty(textToVerify : String[255]) : boolean;
+  var
+    returnValue : boolean;
+    i, emptyChar : integer;
+  Begin
+    emptyChar := 0;
+    returnValue := true;
     
-  Split := returnArray;
-End;
-
-(*Verify if string passed by parameter contains only empty chars*)
-function StringIsEmpty(textToVerify : String[255]) : boolean;
-var
-  returnValue : boolean;
-  i, emptyChar : integer;
-Begin
-  emptyChar := 0;
-  returnValue := true;
+    for i:= 1 to length(textToVerify) do
+    begin
+      returnValue := returnValue and (ord(textToVerify[i]) = emptyChar);
+    end;
+    
+    StringIsEmpty := returnValue;
+  end; 
   
-  for i:= 1 to length(textToVerify) do
+  (*Verify if string starts with given char*)
+  function StringStartsWith(stringToVerify: string[255]; startChar : char) : boolean;
   begin
-    returnValue := returnValue and (ord(textToVerify[i]) = emptyChar);
+    StringStartsWith := stringToVerify[1] = startChar;
   end;
   
-  StringIsEmpty := returnValue;
-end; 
-
-(*Verify if string starts with given char*)
-function StringStartsWith(stringToVerify: string[255]; startChar : char) : boolean;
-begin
-  StringStartsWith := stringToVerify[1] = startChar;
-end;
+  (*Converts string to real. This function was copied from 
+  http://professorwellingtontelles.blogspot.com.br/2011/06/converter-string-em-real-em-pascal.html
+  at 2014-10-07*)
+  function StrToReal (s: String): Real;  
+  var  
+    r : Real;  
+    p : Integer;  
+  begin  
+    Val (s,r,p);  
+    if p > 0 then Val (copy(s,1,p-1),r,p);  
+      StrToReal := r;  
+  end;
+  
+  (*Transforms given string into integer number
+   * and returns its value*)
+  function StrToInteger (s: String) : integer; 
+  var
+    c, returnValue : integer;
+    
+  begin
+    Val(s, returnValue, c);
+    StrToInteger := returnValue;
+  end;
 
 end.
