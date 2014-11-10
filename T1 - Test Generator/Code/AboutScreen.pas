dde819c657a1a72f10574db9196c71bda0e2368a
ricardovsilva <ricardovsilva@msn.com>
2014-11-10 00:06:45 -0200

    Changed About file

--- T1 - Test Generator/Code/AboutScreen.pas
+++ T1 - Test Generator/Code/AboutScreen.pas
@@ -8,14 +8,20 @@ implementation
   begin
     Clrscr;
     Writeln('----------------------------S O B R E----------------------------');
-    Writeln('Este programa foi escrito por Ricardo da Verdade Silva, ');
-    Writeln('aluno da FATEC São Paulo do curso Análise e Desenvolvimento');
+    Writeln('Este programa foi escrito um grupo de alunos ');
+    Writeln('da FATEC São Paulo do curso Análise e Desenvolvimento');
     Writeln('de Sistemas, para a entrega do primeiro trabalho da ');
     Writeln('disciplina de Algoritmo e Lógica de Programação, ministrada ');
     Writeln('pelo Mestre Sérgio Luiz Banin.');
     Writeln('Todo o código fonte deste programa, tal como o registro de ');
     Writeln('commits, e documentação (toda em inglês), estão disponíveis ');
     Writeln('no seguinte endereço: https://github.com/ricardovsilva/fatecsp-alp ');
+    Writeln('Os alunos participantes deste trabalho foram: ');
+    Writeln('- Izaac Silva');
+    Writeln('- Patricia Stankuna Reis');
+    Writeln('- Renan Tiago dos Santos Silva');
+    Writeln('- Rodolpho Pinho');
+    Writeln('- Ricardo da Verdade Silva');
 
     Writeln('');
     Writeln('');
